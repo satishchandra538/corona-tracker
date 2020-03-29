2abe33546c5049733e795d1ad2071c00a63951eb
satish <satishgupta538@gmail.com>
2020-03-29 16:01:29 +0530

    Updates

---
+++ precache-manifest.993d3446f137497d93ec06f8ac3beae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25fc28592a2dc304d1e2162e773dfdf",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "5d33bf8e55c313a06ddf",
+    "url": "/corona-tracker/static/css/main.37d539a2.chunk.css"
+  },
+  {
+    "revision": "dfb4cbebe500821c1e68",
+    "url": "/corona-tracker/static/js/2.9ab9750b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-tracker/static/js/2.9ab9750b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d33bf8e55c313a06ddf",
+    "url": "/corona-tracker/static/js/main.9cdb25f6.chunk.js"
+  },
+  {
+    "revision": "461798bcea028e2f4893",
+    "url": "/corona-tracker/static/js/runtime-main.db302e28.js"
+  }
+]);
